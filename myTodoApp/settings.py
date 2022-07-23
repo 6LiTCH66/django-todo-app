b55b4547a3fbf53edc9bb768b50c3736681c1717
iljah <ilja200303@gmail.com>
2022-07-23 12:45:19 +0300

    code

--- myTodoApp/settings.py
+++ myTodoApp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0nmiohctc9!ut8v0hy)hj^%726(b48h9qa=l$fa#30$fd1+&-0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
